61aa711bb14c26202163c441bc680f51281728e4
kamzhanyue <kamzhanyue@gmail.com>
2025-04-21 16:49:30 +1000

    feat: implement lightning

--- Assets/Scripts/Spells/Lightning.cs
+++ Assets/Scripts/Spells/Lightning.cs
@@ -1,13 +1,19 @@
 using System;
+using UnityEngine;
 
 public class Lightning : MultiTargetSpell
 {
+    private const float HEIGHT_OFFSET = 3f;
+    private ParticleSystem _lightningEffect;
+    private float _knockbackForce;
     protected override void InitConfig(SpellConfig config)
     {
         base.InitConfig(config);
         LightningSpellConfig spellConfig = config as LightningSpellConfig;
         if (spellConfig == null)
             throw new InvalidCastException("Config must be of type LightningSpellConfig.");
+        _lightningEffect = spellConfig.lightingEffect;
+        _knockbackForce = spellConfig.knockbackForce;
     }
 
     protected override void Apply(Enemy enemy)
@@ -17,8 +23,18 @@ public class Lightning : MultiTargetSpell
         {
             effect = DamageEffect.Electrocute;
         }
-        Damage spellDamage = new (damage, DamageType.Electric, effect);
+        Damage spellDamage = new (damage, DamageType.Electric, effect, _knockbackForce);
         enemy.Damage(spellDamage);
+        SpawnParticles(enemy);
+    }
+
+    private void SpawnParticles(Enemy enemy)
+    {
+        ParticleSystem lightning = Instantiate(_lightningEffect);
+        Vector3 spawnPos = enemy.GetCenter();
+        spawnPos.y += HEIGHT_OFFSET;
+        lightning.transform.position = spawnPos;
+        lightning.Play();
     }
 
     protected override bool CanAffect(Enemy enemy)
--- Assets/Scripts/Spells/LightningSpellConfig.cs
+++ Assets/Scripts/Spells/LightningSpellConfig.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Spell Config/Lightning", fileName = "Lightning")]
 public class LightningSpellConfig : SpellConfig
 {
-    
+    public float knockbackForce = 0.5f;
+    public ParticleSystem lightingEffect;
 }
